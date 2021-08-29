5d874ac776f4f038e0bdda9557e4f98347fb2c0b
Bharathrico <bharathganesh26899@gmail.com>
2021-08-28 18:58:57 -0700

    new-push

--- src/components/carosul.vue
+++ src/components/carosul.vue
@@ -2,6 +2,8 @@
     <div class="carosel">
         <h1>Tasks today</h1>
         <div class="innercomponent">
+            <transition name="toggle" appear role="out-in">
+            <transition-group v-if="valueslist.length" name="listelem" appear>
             <div class="listval" v-for="value in valueslist" :key="value.primkey">
                 <div class="numberholder">
                 {{value.primkey}}
@@ -14,6 +16,9 @@
                     <button @click="clickedval(value.primkey)" class="complete">Completed</button>
                 </div>
             </div>
+            </transition-group>
+            <span v-else style="font-size:3vh">oops! no task it seems</span>
+            </transition>
         </div>
     </div>
 </template>
@@ -74,6 +79,8 @@ export default {
     overflow-y: scroll;
     width: 100%;
     height: 80%;
+    position:relative;
+    align-items: center;
     /* background-color: white; */
 }
 .listval
@@ -123,4 +130,68 @@ export default {
     font-size: 2vmin;
     font-weight: 700;
 }
+.listelem-enter-from,
+.listelem-leave-to
+{
+    opacity: 0;
+    transform: scale(0);
+}
+.listelem-enter-to,
+.listelem-leave-from
+{
+    opacity: 1;
+    transform: scale(1);
+}
+.listelem-enter-active
+{
+   transition: all 0.4s ease;
+}
+.listelem-leave-active
+{
+    position: absolute;
+    transition: all 0.4s ease;
+}
+.listelem-move
+{
+    transition: all 0.2s;
+}
+
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+::-webkit-scrollbar-track {
+  background: transparent;
+}
+
+::-webkit-scrollbar-thumb {
+    border-radius: 5px;
+  background: #888;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+
+.toggle-enter-from,
+.toggle-leave-to
+{
+    opacity: 0;
+
+}
+.toggle-enter-to,
+.toggle-leave-from
+{
+    opacity: 1;
+}
+.toggle-enter-active
+{
+   transition: all 0.4s ease;
+}
+.toggle-leave-active
+{
+    position: absolute;
+    transition: all 0.4s ease;
+}
+
 </style>
